91344173f8019dd043d95b099b6487655fa2d278
pransandip <[pransandip@gmail.com]>
2022-09-27 08:11:42 +0530

    Tuesday, september 27, 2022

---
+++ js/arraysEx.js
@@ -0,0 +1,9 @@
+const new$array = new Array(1994, 1995, 1996, 1997)
+console.log(new$array)
+
+new$array[1] = 'sandy'
+console.log(new$array)
+
+const user$name = 'sandy001'
+const person = [user$name, 2022 - 1994, 'coder']
+console.log(person)
---
+++ js/starter.js
@@ -0,0 +1,26 @@
+if (true) console.log("Hello \n\Hunuman 🚀");
+
+if (true) console.log(`Hollo 
+Rama
+`);
+
+// Type coercion :- It's happend whenever operation get value of two different type.
+console.log('23' - '10' - 3) // 10
+console.log('23' + '10' + 3) // 23103 :- it's a string + sign makes any number to string
+console.log('23' * '11') // 253
+console.log('23' / '2') // 11.5
+
+console.log(Number('sandy')) // NaN
+
+
+let n = '1' + 1
+n = n - 1
+console.log(n) // 10
+
+
+// Truthy and Falsy values
+console.log(Boolean(0))  // false
+console.log(Boolean(undefined)) // false
+console.log(Boolean('Laxmana')) // true
+console.log(Boolean({})) // true
+console.log(Boolean('')) // false
